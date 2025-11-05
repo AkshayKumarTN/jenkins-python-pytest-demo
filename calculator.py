5fae8146b1f4efabdfcf193f1495ffcc8978af83
AKSHAY KUMAR TALUR NARASIMMULU <tnak369@gmail.com>
2025-11-05 17:35:06 -0500

    updated calculator

--- calculator.py
+++ calculator.py
@@ -6,6 +6,9 @@ def subtract(a, b):
     """Subtract b from a"""
     return a - b
 
+def multiply(a, b):
+    """Multiply two numbers"""
+    return a * b
 
 def divide(a, b):
     """Divide a by b"""
